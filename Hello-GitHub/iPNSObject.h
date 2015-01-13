c57b4033dbd36c62d8898427be964dda1ce86e84
Ivan Pinzon <ivanpinzon@mac.com>
2015-01-13 11:32:34 -0500

    Added new files
    iPNSObject subclass

---
+++ Hello-GitHub/iPNSObject.h
@@ -0,0 +1,13 @@
+//
+//  iPNSObject.h
+//  Hello-GitHub
+//
+//  Created by Ivan Pinzon on 2015-01-13.
+//  Copyright (c) 2015 Ivan Pinzon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iPNSObject : NSObject
+
+@end
